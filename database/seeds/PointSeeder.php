280eea1662654ee8e7b993f31ba7b487134f833b
Philip Larsson <elt11pla@student.lu.se>
2018-03-04 19:44:10 +0100

    Fix small typo

--- database/seeds/PointSeeder.php
+++ database/seeds/PointSeeder.php
@@ -15,7 +15,7 @@ class PointSeeder extends Seeder
         $fake = Faker::create();
         $multiplierIds = DB::table('multipliers')->pluck('id')->toArray();
 
-        foreach(range(1, 20) as $index) {
+        foreach (range(1, 20) as $index) {
             DB::table('points')->insert([
                 'pie_value' => $fake->numberBetween(1, 20),
                 'multiplier_id' => $fake->randomElement($multiplierIds)
