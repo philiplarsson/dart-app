ee16484b9f1fc2c79ac0379230ab73db0f671489
Philip Larsson <elt11pla@student.lu.se>
2018-05-06 12:05:38 +0200

    Add helpers for signIn when testing

--- tests/APITestCase.php
+++ tests/APITestCase.php
@@ -2,7 +2,9 @@
 
 namespace Tests;
 
+use App\User;
 use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 abstract class APITestCase extends TestCase
 {
@@ -11,11 +13,30 @@ abstract class APITestCase extends TestCase
      * @var Faker
      */
     protected $fake;
+    protected $adminTypeId;
+    protected $userTypeId;
 
     public function setUp()
     {
         parent::setUp();
         $this->seed('TestingDatabaseSeeder');
         $this->fake = Faker::create();
+        $this->adminTypeId = DB::table('user_types')->where('type', 'admin')->first()->id;
+        $this->userTypeId = DB::table('user_types')->where('type', 'user')->first()->id;
     }
+
+    protected function signIn($user = null)
+    {
+        $user = $user ?: factory(User::class)->create(['type_id' => $this->userTypeId]);
+
+        $this->actingAs($user);
+
+        return $this;
+    }
+
+    protected function signInAsAdmin()
+    {
+        return $this->signIn(factory(User::class)->create(['type_id' => $this->adminTypeId]));
+    }
+
 }
